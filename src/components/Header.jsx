7000b744123bf3b3941baeebaf29caa896948361
CedricA <cedric.j.aoun@gmail.com>
2025-05-12 14:41:27 +0200

    act 7: added useMatch for categories

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
-import { NavLink } from "react-router"
+import { NavLink, useMatch } from "react-router"
 import '../styles/header.css'
 
 function Header() {
+  const match = useMatch("/category/:categoryId");
   return (
     <header>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav className="ms-auto d-flex align-items-center">
+              {match && <div className="text-primary">Category: {match.params.categoryId}</div> }
               <Nav.Link to="/" as={NavLink}>Acceuil</Nav.Link>
               <Nav.Link to="/category/clothes" as={NavLink}>Vêtements</Nav.Link>
               <Nav.Link to="/category/shoes" as={NavLink}>Chaussures</Nav.Link>
