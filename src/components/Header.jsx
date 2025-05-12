b5e280dd0c564571fad6bc5077e210556a6d5e9d
CedricA <cedric.j.aoun@gmail.com>
2025-05-12 15:04:56 +0200

    act 8: added button to route with useNavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto d-flex align-items-center">
-              {match && <div className="text-primary">Category: {match.params.categoryId}</div> }
+              {match && <div className="text-primary mx-3">Category: {match.params.categoryId}</div> }
               <Nav.Link to="/" as={NavLink}>Acceuil</Nav.Link>
               <Nav.Link to="/category/clothes" as={NavLink}>Vêtements</Nav.Link>
               <Nav.Link to="/category/shoes" as={NavLink}>Chaussures</Nav.Link>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router"
+import { Button } from "react-bootstrap"
+import { useNavigate, useParams } from "react-router"
 
 function Category() {
-  const { categoryId } = useParams()
+  const { categoryId } = useParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  }
 
   return (
-    <h1>Details de la page avec la category: {categoryId}</h1>
+    <>
+      <h1>Details de la page avec la category: {categoryId}</h1>
+      <Button onClick={handleClick}>Retour à la page d'acceuil</Button>
+    </>
   )
 }
 
